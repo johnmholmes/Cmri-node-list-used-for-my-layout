e4a3ecb16ee2cc0cc628ec606f687b6673e53db3
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2023-04-13 11:27:25 +0100

    Update NODE_4.ino

--- NODE_4/NODE_4.ino
+++ NODE_4/NODE_4.ino
@@ -14,23 +14,20 @@
 #define CMRI_ADDR 4
 
 // define turnout required positions each turnout will need calibrating
-#define turnout1ClosedPosition 108
-#define turnout1ThrownPosition 80
+#define turnout1ClosedPosition 110
+#define turnout1ThrownPosition 35
 
-// define signal leds turnout 1 pins 0 and 1 rx & tx ADDRESS 3001 USED FOR BIT STATE OF TURNOUT
-#define throughApproachGreenLed 4                 //jmri 4002
-#define throughApproachRedLed 5                   //jmri 4003
-#define divergingApproachGreenLed 6               //jmri 4004
-#define divergingApproachRedLed 7                 //jmri 4005
-#define throughGreenLed 8                         //jmri 4006
-#define throughRedLed 9                           //jmri 4007
+#define lightControl 4                            //jmri 4002
 
-// Spare pins at the moment 3 10 to 13 And A3 to A7 
+
+// Spare pins at the moment 
 
 //define infrared detection T3 as inputs
 #define Sensor1 A0                                //jmri 4002
 #define Sensor2 A1                                //jmri 4003
 #define Sensor3 A2                                //jmri 4004
+#define Sensor3 A3                                //jmri 4005
+#define Sensor3 A3                                //jmri 4005
 
 //setup themove speed of servo
 #define turnoutMoveSpeed 20                       // [ms] lower number is faster
@@ -48,17 +45,14 @@ byte turnout1Target   = turnout1ClosedPosition;
 void setup() {
   delay(2000);
   //setup output pins
-  pinMode(throughApproachGreenLed, OUTPUT);
-  pinMode(throughApproachRedLed, OUTPUT);
-  pinMode(divergingApproachGreenLed, OUTPUT);
-  pinMode(divergingApproachRedLed, OUTPUT);
-  pinMode(throughGreenLed, OUTPUT);
-  pinMode(throughRedLed, OUTPUT);
+  pinModee(lightControl, OUTPUT);
 
   //setup input pins
   pinMode(Sensor1, INPUT_PULLUP);
   pinMode(Sensor2, INPUT_PULLUP);
   pinMode(Sensor3, INPUT_PULLUP);
+  pinMode(Sensor4, INPUT_PULLUP);
+  pinMode(Sensor5, INPUT_PULLUP);
 
 
   digitalWrite(throughApproachGreenLed, LOW);      // light led 1 on start up  to show its working
@@ -85,18 +79,16 @@ void loop() {
     cmri.set_bit(0, LOW);                                                   //4001
   }
 
-  // read the cmri bit and switch on led ! used as i am using + commom leds tmc signals
-  digitalWrite (throughApproachGreenLed, !cmri.get_bit(1));                 //jmri SH1 4002 
-  digitalWrite (throughApproachRedLed, !cmri.get_bit(2));                   //jmri SH1 4003
-  digitalWrite (divergingApproachGreenLed, !cmri.get_bit(3));               //jmri SH2 4004
-  digitalWrite (divergingApproachRedLed, !cmri.get_bit(4));                 //jmri SH2 4005
-  digitalWrite (throughGreenLed, !cmri.get_bit(5));                         //jmri SH3 4006
-  digitalWrite (throughRedLed, !cmri.get_bit(6));                           //jmri SH3 4007
+  // read the cmri bit and switch on led 
+  digitalWrite (lightControl, cmri.get_bit(1));                             //jmri SH1 4002 
+
 
   // get senors status and send to jmri
   cmri.set_bit(1, !digitalRead(A0));                                        //jmri 4002
   cmri.set_bit(2, !digitalRead(A1));                                        //jmri 4003
   cmri.set_bit(3, !digitalRead(A2));                                        //jmri 4004
+  cmri.set_bit(4, !digitalRead(A2));                                        //jmri 4005
+  cmri.set_bit(5, !digitalRead(A2));                                        //jmri 4006
 
 
   if (turnout1Position != turnout1Target) {
