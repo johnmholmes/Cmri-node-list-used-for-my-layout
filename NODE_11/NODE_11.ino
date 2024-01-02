a8471d7678cf158b20d4c0624bbc5c6c50d9c4b5
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2024-01-02 18:38:06 +0000

    code update

--- NODE_11/NODE_11.ino
+++ NODE_11/NODE_11.ino
@@ -5,32 +5,32 @@
   the servo has been moved
   also will control 6 off signal leds
   it will also read 2 sensors/switchs
-  Code uses some of Chris Sharp code to add slow motion servo.
+
 */
 #include <CMRI.h>
 #include <Servo.h>
 
 // define node address each node needs its own address
-#define CMRI_ADDR 3
+#define CMRI_ADDR 11
 
 // define turnout required positions each turnout will need calibrating
 #define turnout1ClosedPosition 108
 #define turnout1ThrownPosition 80
 
-// define signal leds turnout 1 pins 0 and 1 rx & tx ADDRESS 3001 USED FOR BIT STATE OF TURNOUT
-#define throughApproachGreenLed 4                 //jmri 3002
-#define throughApproachRedLed 5                   //jmri 3003
-#define divergingApproachGreenLed 6               //jmri 3004
-#define divergingApproachRedLed 7                 //jmri 3005
-#define throughGreenLed 8                         //jmri 3006
-#define throughRedLed 9                           //jmri 3007
+// define signal leds turnout 1 pins 0 and 1 rx & tx ADDRESS 11001 USED FOR BIT STATE OF TURNOUT
+#define throughApproachGreenLed 4                 //jmri 11002
+#define throughApproachRedLed 5                   
+#define divergingApproachGreenLed 6               
+#define divergingApproachRedLed 7                 
+#define throughGreenLed 8                         
+#define throughRedLed 9                           
 
 // Spare pins at the moment 3 10 to 13 And A3 to A7 
 
-//define infrared detection T3 as inputs
-#define Sensor1 A0                                //jmri 3002
-#define Sensor2 A1                                //jmri 3003
-#define Sensor3 A2                                //jmri 3004
+//define infrared detection T11 as inputs
+#define Sensor1 A0                                //jmri 11002
+#define Sensor2 A1                                //jmri 11003
+#define Sensor3 A2                                //jmri 11004
 
 //setup themove speed of servo
 #define turnoutMoveSpeed 20                       // [ms] lower number is faster
@@ -72,31 +72,31 @@ void setup() {
 
 void loop() {
   cmri.process();
-  turnout1 = (cmri.get_bit(0));                                             //turnout 3001 jmri address
+  turnout1 = (cmri.get_bit(0));                                             //turnout 11001 jmri address
 
   if (turnout1 == 1 && t1state == 0) {
     turnout1Target = turnout1ThrownPosition;   
     t1state = 1;
-    cmri.set_bit(0, HIGH);                                                  //3001 jmri address output
+    cmri.set_bit(0, HIGH);                                                  //11001 jmri address output
   }
   if (turnout1 == 0 && t1state == 1) {
     turnout1Target = turnout1ClosedPosition;    
     t1state = 0;
-    cmri.set_bit(0, LOW);                                                   //3001
+    cmri.set_bit(0, LOW);                                                   //11001
   }
 
   // read the cmri bit and switch on led ! used as i am using + commom leds tmc signals
-  digitalWrite (throughApproachGreenLed, !cmri.get_bit(1));                 //jmri SH1 3002 
-  digitalWrite (throughApproachRedLed, !cmri.get_bit(2));                   //jmri SH1 3003
-  digitalWrite (divergingApproachGreenLed, !cmri.get_bit(3));               //jmri SH2 3004
-  digitalWrite (divergingApproachRedLed, !cmri.get_bit(4));                 //jmri SH2 3005
-  digitalWrite (throughGreenLed, !cmri.get_bit(5));                         //jmri SH3 3006
-  digitalWrite (throughRedLed, !cmri.get_bit(6));                           //jmri SH3 3007
+  digitalWrite (throughApproachGreenLed, !cmri.get_bit(1));                 //jmri SH1 11002 
+  digitalWrite (throughApproachRedLed, !cmri.get_bit(2));
+  digitalWrite (divergingApproachGreenLed, !cmri.get_bit(3));
+  digitalWrite (divergingApproachRedLed, !cmri.get_bit(4));
+  digitalWrite (throughGreenLed, !cmri.get_bit(5));
+  digitalWrite (throughRedLed, !cmri.get_bit(6));                           
 
   // get senors status and send to jmri
-  cmri.set_bit(1, !digitalRead(A0));                                        //jmri 3002
-  cmri.set_bit(2, !digitalRead(A1));                                        //jmri 3003
-  cmri.set_bit(3, !digitalRead(A2));                                        //jmri 3004
+  cmri.set_bit(1, !digitalRead(A0));                                        //jmri 11002
+  cmri.set_bit(2, !digitalRead(A1)); 
+  cmri.set_bit(3, !digitalRead(A2));
 
 
   if (turnout1Position != turnout1Target) {
